a5827aa5e40c4d7241ce79f26c12ba946c09867e
wangjianjing <jianjing.wang@spddw.com>
2025-06-23 11:57:41 +0800

    fix: 移除未使用的合同解析页面路由定义，简化路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -16,11 +16,6 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/contract-parsing',
-      name: 'contract-parsing',
-      component: () => import('../views/ContractParsingView.vue')
     }
   ]
 })
